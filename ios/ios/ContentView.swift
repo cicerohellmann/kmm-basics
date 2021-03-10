84130b06d13619407537ee8ea663d153bfcbe5df
Cicero Hellmann <cicerohellmann@gmail.com>
2021-03-10 09:23:04 +0100

    Login with basic navigation

--- ios/ios/ContentView.swift
+++ ios/ios/ContentView.swift
@@ -6,12 +6,26 @@ func greet() -> String {
 }
 
 struct ContentView: View {
+    @State private var login: String = ""
+    @State private var password: String = ""
+    
     var body: some View {
-        VStack(alignment: .leading
-        ) {
-            Spacer()
-            Text(greet())
-            Text(greet())
+        NavigationView {
+            VStack(alignment: .center
+            ) {
+                Spacer()
+                Spacer()
+                TextField("Login", text: $login).multilineTextAlignment(.center)
+                TextField("Password", text: $password).multilineTextAlignment(.center)
+                Spacer()
+                Button(action: {
+                }) {
+                    NavigationLink(destination: DestinyView()) {
+                        Text("Login")
+                    }
+                }
+                Spacer()
+            }
         }
     }
 }
@@ -19,5 +33,16 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct DestinyView: View {
+    var body: some View {
+        NavigationView {
+            VStack(alignment: .center
+            ) {
+                Text("NavigationTest")
+            }
+        }.navigationBarHidden(true)
     }
 }
